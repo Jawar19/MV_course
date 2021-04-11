256bf7d759678d88e1ba18b1331e9969c07a41da
jacobXPS <Jawar19@student.sdu.dk>
2021-04-11 17:35:12 +0200

    added if statements to run select portions of code

--- main.py
+++ main.py
@@ -16,79 +16,85 @@ path = os.path.normpath(os.path.join(fileDir, relPath))
 
 print("\n__--I am located at: ", fileDir, "--__")
 
-## opgave 1
+opg = 1 # vælg opgave nummer her, inden i afvikler koden
 
-# TODO: indlæs billede fra mappen "img/BabyFood/" og vis dette i et "named window"
-#   HINT: imread() og imshow()
+if opg == 1:
+    ## opgave 1 & 2
 
-
-cv.waitKey(0)
-
-# TODO: segmenter billedet fra "img/BabyFood" så kun de røde skeer vises.
-#   HINT: brug forloop og if-statements
+    # TODO: indlæs billede fra mappen "img/BabyFood/" og vis dette i et "named window"
+    #   HINT: imread() og imshow()
 
 
-cv.waitKey(0)
+    cv.waitKey(0)
 
-## Opgave 3
-
-# I opgave 3 skal i ved hjælp af HAAR-Cascades finde ansigtet på et billede og markere dette.
-#   Det kan bla. gøres med rectangle eller circle.
-
-# load af haar-like features
-cascPath = cv.data.haarcascades
-
-facePath = cascPath + "haarcascade_frontalface_default.xml"
-faceCasc = cv.CascadeClassifier(facePath)
-
-# TODO: indlæs selv "eye" og "smile" haar-cascades
+    # TODO: segmenter billedet fra "img/BabyFood" så kun de røde skeer vises.
+    #   HINT: brug forloop og if-statements
 
 
+    cv.waitKey(0)
 
-# TODO: indlæs billedet "face.jpg" fra mappen "/img/" i farve
+if opg == 3:
+    ## Opgave 3
 
+    # I opgave 3 skal i ved hjælp af HAAR-Cascades finde ansigtet på et billede og markere dette.
+    #   Det kan bla. gøres med rectangle eller circle.
 
+    # load af haar-like features
+    cascPath = cv.data.haarcascades
 
-# TODO: opret en kopi af det indlæste billede og konverter dette til gråtoner
-#   HINT: cv.CVTCOLOR og flaget cv.COLOR_BGR2GRAY
+    facePath = cascPath + "haarcascade_frontalface_default.xml"
+    faceCasc = cv.CascadeClassifier(facePath)
 
-
-faces = faceCascade.detectMultiScale(
-    gray,
-    scaleFactor=1.1,
-    minNeighbors=5,
-    minSize=(30,30),
-    flags=cv.CASCADE_SCALE_IMAGE
-)
-
-for (x, y, w, h) in faces:
-    # TODO: make "Region of interest" af det originale og gråtone billedet, som kan bruges til at finde
-    #   smil og øjne.
-    roiColor = color[y:y+h, x:x+w]
-    roiGray
-
-    # TODO: tegn en figur som markerer ansigtet, f.eks. rectangle.
-    cv.rectangle(frame,
-                 """position af øverste venstre hjørne""",
-                 """position af nederste højre hjørne""",
-                 (255, 0, 0),   #farven i BGR værdier
-                 3)             #tykkelse på outline
+    # TODO: indlæs selv "eye" og "smile" haar-cascades
 
 
 
-## opgave 4
+    # TODO: indlæs billedet "face.jpg" fra mappen "/img/" i farve
 
-# TODO: Lav et videocapture objekt med cv.VideoCapture()
-cap =
 
-while True:
-    ret, frame = cap.read()
 
-    # TODO: Implemeter facedetection som ovenfor, og opdater vinduet "Video"
-    cv.imshow("video", frame)
+    # TODO: opret en kopi af det indlæste billede og konverter dette til gråtoner
+    #   HINT: cv.CVTCOLOR og flaget cv.COLOR_BGR2GRAY
 
-    if cv.waitKey(25) & 0xFF == ord('q'): # stop videovisning ved at trykke på "q"
-            break
 
-cap.release() #release videodevice, så systemet kan se den igen
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(30,30),
+        flags=cv.CASCADE_SCALE_IMAGE
+    )
+
+    for (x, y, w, h) in faces:
+        # TODO: make "Region of interest" af det originale og gråtone billedet, som kan bruges til at finde
+        #   smil og øjne.
+        roiColor = color[y:y+h, x:x+w]
+        roiGray
+
+        # TODO: tegn en figur som markerer ansigtet, f.eks. rectangle.
+        cv.rectangle(frame,
+                     """position af øverste venstre hjørne""",
+                     """position af nederste højre hjørne""",
+                     (255, 0, 0),   #farven i BGR værdier
+                     3)             #tykkelse på outline
+
+
+if opg == 4:
+    ## opgave 4
+
+    # TODO: Lav et videocapture objekt med cv.VideoCapture()
+    cap =
+
+    while True:
+        ret, frame = cap.read()
+
+        # TODO: Implemeter facedetection som ovenfor, og opdater vinduet "Video"
+        cv.imshow("video", frame)
+
+        if cv.waitKey(25) & 0xFF == ord('q'): # stop videovisning ved at trykke på "q"
+                break
+
+    cap.release() #release videodevice, så systemet kan se den igen
+
+
 cv.destroyAllWindows() # lukker alle video vinduer.
